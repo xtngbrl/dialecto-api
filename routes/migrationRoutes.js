22d7d92e4a0a9df6a19996a6a20440ed185ce126
ChristianBernardino <133891675+ChristianBernardino@users.noreply.github.com>
2025-06-23 00:31:19 +0800

    Update migrationRoutes.js

--- routes/migrationRoutes.js
+++ routes/migrationRoutes.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const migrationController = require('../controllers/migrationController');
 
 const authorize = require('../middleware/authorizationMiddleware'); 
-const { permit } = require('../middleware/permissionMiddleware');
+const { permission } = require('../middleware/permissionMiddleware');
 router.use(authorize);
 // Migration endpoints
-router.post('/migrations/up', permit(['Admin']), migrationController.migrateUp);
-router.post('/migrations/down', permit(['Admin']), migrationController.migrateDown);
-router.post('/migrations/undo-all', permit(['Admin']), migrationController.migrateUndoAll);
+router.post('/migrations/up', permission(['Admin']), migrationController.migrateUp);
+router.post('/migrations/down', permission(['Admin']), migrationController.migrateDown);
+router.post('/migrations/undo-all', permission(['Admin']), migrationController.migrateUndoAll);
 
 // Seeder endpoints
-router.post('/seeders/up', permit(['Admin']), migrationController.seedUp);
-router.post('/seeders/down', permit(['Admin']), migrationController.seedDown);
-router.post('/seeders/undo-all', permit(['Admin']), migrationController.seedUndoAll);
+router.post('/seeders/up', permission(['Admin']), migrationController.seedUp);
+router.post('/seeders/down', permission(['Admin']), migrationController.seedDown);
+router.post('/seeders/undo-all', permission(['Admin']), migrationController.seedUndoAll);
 
 module.exports = router;
